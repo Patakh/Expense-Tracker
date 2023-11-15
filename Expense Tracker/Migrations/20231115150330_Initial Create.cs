ca4670d30b284eeab397cccc89807762070923ca
Patakh <108473337+Patakh@users.noreply.github.com>
2023-11-15 18:10:23 +0300

    crrrrrrrrrrrreate

---
+++ Expense Tracker/Migrations/20231115150330_Initial Create.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expense_Tracker.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CategorySet",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Icon = table.Column<string>(type: "nvarchar(5)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategorySet", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionSet",
+                columns: table => new
+                {
+                    TransactionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(75)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionSet", x => x.TransactionId);
+                    table.ForeignKey(
+                        name: "FK_TransactionSet_CategorySet_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "CategorySet",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransactionSet_CategoryId",
+                table: "TransactionSet",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TransactionSet");
+
+            migrationBuilder.DropTable(
+                name: "CategorySet");
+        }
+    }
+}
